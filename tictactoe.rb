2e4cc05c091a165d5186b8f31efcfb318bdd74e5
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-09-29 15:36:45 +0100

    Refactor: Rename var and use 2D array for session vars

--- tictactoe.rb
+++ tictactoe.rb
@@ -2,6 +2,9 @@ require "sinatra/base"
 require "erb"
 
 class MyApp < Sinatra::Base
+
+    @@session_vars = Array.new(3){ Array.new(3) { "" } }
+
     enable :sessions
 
     get '/tictactoe' do
@@ -20,27 +23,32 @@ class MyApp < Sinatra::Base
     private
 
     def update_session_vars_from_inputs
-        session[:row1_col1] = params["row1_col1_in"]
-        session[:row1_col2] = params["row1_col2_in"]
-        session[:row1_col3] = params["row1_col3_in"]
-        session[:row2_col1] = params["row2_col1_in"]
-        session[:row2_col2] = params["row2_col2_in"]
-        session[:row2_col3] = params["row2_col3_in"]
-        session[:row3_col1] = params["row3_col1_in"]
-        session[:row3_col2] = params["row3_col2_in"]
-        session[:row3_col3] = params["row3_col3_in"]
+        @@session_vars[0][0] = params["row1_col1_in"]
+        @@session_vars[0][1] = params["row1_col2_in"]
+        @@session_vars[0][2] = params["row1_col3_in"]
+        @@session_vars[1][0] = params["row2_col1_in"]
+        @@session_vars[1][1] = params["row2_col2_in"]
+        @@session_vars[1][2] = params["row2_col3_in"]
+        @@session_vars[2][0] = params["row3_col1_in"]
+        @@session_vars[2][1] = params["row3_col2_in"]
+        @@session_vars[2][2] = params["row3_col3_in"]
+
+        session[:cell_values] = @@session_vars
     end
 
     def update_template_vars_from_session
-        @rowcol = Array.new(3){ Array.new(3) { "" } }
-        @rowcol[0][0] = session[:row1_col1]
-        @rowcol[0][1] = session[:row1_col2]
-        @rowcol[0][2] = session[:row1_col3]
-        @rowcol[1][0] = session[:row2_col1]
-        @rowcol[1][1] = session[:row2_col2]
-        @rowcol[1][2] = session[:row2_col3]
-        @rowcol[2][0] = session[:row3_col1]
-        @rowcol[2][1] = session[:row3_col2]
-        @rowcol[2][2] = session[:row3_col3]
+        @cells = Array.new(3){ Array.new(3) { "" } }
+        # @cells.each do |row|
+        #     row.each do |cell|
+        @cells[0][0] = session[:cell_values][0][0]
+        @cells[0][1] = session[:cell_values][0][1]
+        @cells[0][2] = session[:cell_values][0][2]
+        @cells[1][0] = session[:cell_values][1][0]
+        @cells[1][1] = session[:cell_values][1][1]
+        @cells[1][2] = session[:cell_values][1][2]
+        @cells[2][0] = session[:cell_values][2][0]
+        @cells[2][1] = session[:cell_values][2][1]
+        @cells[2][2] = session[:cell_values][2][2]
+        # end
     end
 end
