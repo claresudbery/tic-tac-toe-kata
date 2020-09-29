b3a1b86c4514c4624cb95334dfe80cb3692b834a
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-09-29 14:58:34 +0100

    Refactor: Use methods to set lists of values

--- tictactoe.rb
+++ tictactoe.rb
@@ -5,19 +5,21 @@ class MyApp < Sinatra::Base
     enable :sessions
 
     get '/tictactoe' do
-        @row1_col1 = session[:row1_col1]
-        @row2_col1 = session[:row2_col1]
-        @row3_col1 = session[:row3_col1]
-        @row1_col2 = session[:row1_col2]
-        @row2_col2 = session[:row2_col2]
-        @row3_col2 = session[:row3_col2]
-        @row1_col3 = session[:row1_col3]
-        @row2_col3 = session[:row2_col3]
-        @row3_col3 = session[:row3_col3]
+        update_template_vars_from_session
         erb :tictactoe
     end
 
     post "/tictactoe" do
+        update_session_vars_from_inputs
+        update_template_vars_from_session
+        erb :tictactoe
+    end
+
+    run! if app_file == $0
+
+    private
+
+    def update_session_vars_from_inputs
         session[:row1_col1] = params["row1_col1_in"]
         session[:row2_col1] = params["row2_col1_in"]
         session[:row3_col1] = params["row3_col1_in"]
@@ -27,6 +29,9 @@ class MyApp < Sinatra::Base
         session[:row1_col3] = params["row1_col3_in"]
         session[:row2_col3] = params["row2_col3_in"]
         session[:row3_col3] = params["row3_col3_in"]
+    end
+
+    def update_template_vars_from_session
         @row1_col1 = session[:row1_col1]
         @row2_col1 = session[:row2_col1]
         @row3_col1 = session[:row3_col1]
@@ -36,8 +41,5 @@ class MyApp < Sinatra::Base
         @row1_col3 = session[:row1_col3]
         @row2_col3 = session[:row2_col3]
         @row3_col3 = session[:row3_col3]
-        erb :tictactoe
     end
-
-    run! if app_file == $0
 end
