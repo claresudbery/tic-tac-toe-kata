87eeab1c0633dff58ddc1f60d1f49374fca7be38
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-09-29 17:43:54 +0100

    Use for loop for populating session vars

--- tictactoe.rb
+++ tictactoe.rb
@@ -25,15 +25,11 @@ class MyApp < Sinatra::Base
             session[:cell_values] = Array.new(3){ Array.new(3) { "" } }
         end
 
-        session[:cell_values][0][0] = params["row1_col1_in"]
-        session[:cell_values][0][1] = params["row1_col2_in"]
-        session[:cell_values][0][2] = params["row1_col3_in"]
-        session[:cell_values][1][0] = params["row2_col1_in"]
-        session[:cell_values][1][1] = params["row2_col2_in"]
-        session[:cell_values][1][2] = params["row2_col3_in"]
-        session[:cell_values][2][0] = params["row3_col1_in"]
-        session[:cell_values][2][1] = params["row3_col2_in"]
-        session[:cell_values][2][2] = params["row3_col3_in"]
+        for row in 0..2 
+            for col in 0..2 
+                session[:cell_values][row][col] = params["row#{row+1}_col#{col+1}_in"]
+            end
+        end
     end
 
     def update_template_vars_from_session
