d7039bc051be694d7ac62466a079f574956706c2
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-09-29 13:14:41 +0100

    Now remembers plays made by users

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -13,17 +13,41 @@ RSpec.describe 'The HelloWorld App' do
       MyApp
     end
   
-    it "starts with an empty 3x3 tic-tac-toe grid" do
-      get '/tictactoe'
-      expect(last_response).to be_ok
-      expect(last_response.body).to have_tag('input', :with => { :class => 'row1 col1' })
-      expect(last_response.body).to have_tag('input', :with => { :class => 'row1 col2' })
-      expect(last_response.body).to have_tag('input', :with => { :class => 'row1 col3' })
-      expect(last_response.body).to have_tag('input', :with => { :class => 'row2 col1' })
-      expect(last_response.body).to have_tag('input', :with => { :class => 'row2 col2' })
-      expect(last_response.body).to have_tag('input', :with => { :class => 'row2 col3' })
-      expect(last_response.body).to have_tag('input', :with => { :class => 'row3 col1' })
-      expect(last_response.body).to have_tag('input', :with => { :class => 'row3 col2' })
-      expect(last_response.body).to have_tag('input', :with => { :class => 'row3 col3' })
+    context "get /tictactoe" do  
+      it "starts with an empty 3x3 tic-tac-toe grid" do
+        get '/tictactoe'
+        expect(last_response).to be_ok
+        expect(last_response.body).to have_tag('input', :with => { :class => 'row1 col1' })
+        expect(last_response.body).to have_tag('input', :with => { :class => 'row1 col2' })
+        expect(last_response.body).to have_tag('input', :with => { :class => 'row1 col3' })
+        expect(last_response.body).to have_tag('input', :with => { :class => 'row2 col1' })
+        expect(last_response.body).to have_tag('input', :with => { :class => 'row2 col2' })
+        expect(last_response.body).to have_tag('input', :with => { :class => 'row2 col3' })
+        expect(last_response.body).to have_tag('input', :with => { :class => 'row3 col1' })
+        expect(last_response.body).to have_tag('input', :with => { :class => 'row3 col2' })
+        expect(last_response.body).to have_tag('input', :with => { :class => 'row3 col3' })
+      end
+    end
+  
+    context "post /tictactoe" do
+      grid_cells = {
+        :row1_col1_in => 'input.row1.col1',
+        :row2_col1_in => 'input.row2.col1',
+        :row3_col1_in => 'input.row3.col1',
+        :row1_col2_in => 'input.row1.col2',
+        :row2_col2_in => 'input.row2.col2',
+        :row3_col2_in => 'input.row3.col2',
+        :row1_col3_in => 'input.row1.col3',
+        :row2_col3_in => 'input.row2.col3',
+        :row3_col3_in => 'input.row3.col3'
+      }
+
+      grid_cells.each do |input, css|
+        it "remembers when user plays O or X" do
+          post "/tictactoe", input => "X" 
+          expect(last_response).to be_ok
+          expect(last_response.body).to have_tag(css, :value => 'X')
+        end
+      end
     end
 end
--- tictactoe.rb
+++ tictactoe.rb
@@ -5,6 +5,37 @@ class MyApp < Sinatra::Base
     enable :sessions
 
     get '/tictactoe' do
+        @row1_col1 = session[:row1_col1]
+        @row2_col1 = session[:row2_col1]
+        @row3_col1 = session[:row3_col1]
+        @row1_col2 = session[:row1_col2]
+        @row2_col2 = session[:row2_col2]
+        @row3_col2 = session[:row3_col2]
+        @row1_col3 = session[:row1_col3]
+        @row2_col3 = session[:row2_col3]
+        @row3_col3 = session[:row3_col3]
+        erb :tictactoe
+    end
+
+    post "/tictactoe" do
+        session[:row1_col1] = params["row1_col1_in"]
+        session[:row2_col1] = params["row2_col1_in"]
+        session[:row3_col1] = params["row3_col1_in"]
+        session[:row1_col2] = params["row1_col2_in"]
+        session[:row2_col2] = params["row2_col2_in"]
+        session[:row3_col2] = params["row3_col2_in"]
+        session[:row1_col3] = params["row1_col3_in"]
+        session[:row2_col3] = params["row2_col3_in"]
+        session[:row3_col3] = params["row3_col3_in"]
+        @row1_col1 = session[:row1_col1]
+        @row2_col1 = session[:row2_col1]
+        @row3_col1 = session[:row3_col1]
+        @row1_col2 = session[:row1_col2]
+        @row2_col2 = session[:row2_col2]
+        @row3_col2 = session[:row3_col2]
+        @row1_col3 = session[:row1_col3]
+        @row2_col3 = session[:row2_col3]
+        @row3_col3 = session[:row3_col3]
         erb :tictactoe
     end
 
