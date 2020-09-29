a83d917993009f065caba0a3f74b7a2fb6a0d254
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-09-29 10:14:00 +0100

    Removed the last vestiges of the monstas app this was copied from.

--- config.ru
+++ config.ru
@@ -1,5 +1,2 @@
-# To start the app, run the following on the command line: rackup -p 4567
-# Alternatively you can still just use: ruby monstas.rb
-
-require './monstas'
+require './tictactoe'
 run MyApp
--- tictactoe.rb
+++ tictactoe.rb
@@ -1,11 +1,6 @@
 require "sinatra/base"
 require "erb"
 
-# This page created as part of this tutorial: http://webapps-for-beginners.rubymonstas.org/sinatra/params.html
-
-# To start the app / spin up the server, run the following on the command line: rackup -p 4567
-# Alternatively you can still just use: ruby monstas.rb
-
 class MyApp < Sinatra::Base
     enable :sessions
 
