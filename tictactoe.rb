60f584d1bc91232c4b223b22470b254e16f0373d
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-09-29 15:15:18 +0100

    Use 2-dimensional array instead of individually-named template vars

--- tictactoe.rb
+++ tictactoe.rb
@@ -32,14 +32,15 @@ class MyApp < Sinatra::Base
     end
 
     def update_template_vars_from_session
-        @row1_col1 = session[:row1_col1]
-        @row2_col1 = session[:row2_col1]
-        @row3_col1 = session[:row3_col1]
-        @row1_col2 = session[:row1_col2]
-        @row2_col2 = session[:row2_col2]
-        @row3_col2 = session[:row3_col2]
-        @row1_col3 = session[:row1_col3]
-        @row2_col3 = session[:row2_col3]
-        @row3_col3 = session[:row3_col3]
+        @rowcol = Array.new(3){ Array.new(3) { "" } }
+        @rowcol[0][0] = session[:row1_col1]
+        @rowcol[1][0] = session[:row2_col1]
+        @rowcol[2][0] = session[:row3_col1]
+        @rowcol[0][1] = session[:row1_col2]
+        @rowcol[1][1] = session[:row2_col2]
+        @rowcol[2][1] = session[:row3_col2]
+        @rowcol[0][2] = session[:row1_col3]
+        @rowcol[1][2] = session[:row2_col3]
+        @rowcol[2][2] = session[:row3_col3]
     end
 end
