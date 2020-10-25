2782efca498d01b84dcac01b15d9ee3a76996eb2
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-10-25 16:07:53 +0000

    Refactor: Cleaner code.

--- lib/tictactoe_logic.rb
+++ lib/tictactoe_logic.rb
@@ -3,15 +3,21 @@ class TicTacToeLogic
         winner = nil
         i = 0
 
-        while winner == nil && i < 3
-            if !grid_cells[i][0].nil? && !grid_cells[i][0].empty? \
-                    && (grid_cells[i][0] == grid_cells[i][1]) \
-                    && (grid_cells[i][1] == grid_cells[i][2])
-                winner = grid_cells[i][0]
-            end  
+        while winner == nil && i < 3 
+            winner = find_horizontal_winner(grid_cells[i])
             i = i + 1
         end
 
         winner
+    end
+
+    private 
+
+    def find_horizontal_winner(grid_row)
+        if !grid_row[0].nil? && !grid_row[0].empty? \
+                && (grid_row[0] == grid_row[1]) \
+                && (grid_row[1] == grid_row[2])
+            winner = grid_row[0]
+        end 
     end
 end
