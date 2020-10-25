7b6c440f8bac403d7933c4267754b0f95d245037
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-10-25 16:13:31 +0000

    Find winners on a vertical row.

--- lib/tictactoe_logic.rb
+++ lib/tictactoe_logic.rb
@@ -2,9 +2,11 @@ class TicTacToeLogic
     def get_winner(grid_cells)
         winner = nil
         i = 0
+        transposed_grid = grid_cells.transpose
 
         while winner == nil && i < 3 
             winner = find_horizontal_winner(grid_cells[i])
+            winner = winner.nil? ? find_horizontal_winner(transposed_grid[i]) : winner
             i = i + 1
         end
 
--- spec/tictactoe_logic_spec.rb
+++ spec/tictactoe_logic_spec.rb
@@ -2,36 +2,40 @@ require "spec_helper"
 
 RSpec.describe 'The tic-tac-toe App' do
     context "deciding if somebody has won" do 
-        winning_game_grids = [
+        horizontal_winning_rows = [
             ["X",  [["X", "X", "X"],
-                    ["",  "",  ""],
-                    ["",  "",  ""]]],
-            ["O",  [["O", "O", "O"],
                     ["",  "",  ""],
                     ["",  "",  ""]]],
             ["X",  [["",  "",  ""],
                     ["X", "X", "X"],
                     ["",  "",  ""]]],
-            ["O",  [["",  "",  ""],
-                    ["O", "O", "O"],
-                    ["",  "",  ""]]],
             ["X",  [["",  "",  ""],
                     ["",  "",  ""],
-                    ["X", "X", "X"]]],
-            ["O",  [["",  "",  ""],
-                    ["",  "",  ""],
-                    ["O", "O", "O"]]]
+                    ["X", "X", "X"]]]
         ]
 
         # Arrange                
-        winning_game_grids.each do |player, grid_cells|
-            it "can detect when #{player} has won with grid #{grid_cells}" do
+        horizontal_winning_rows.each do |player, grid_cells|
+            it "can detect when #{player} has won with a horizontal row, like this: #{grid_cells}" do
                 # Act
                 result = TicTacToeLogic.new.get_winner(grid_cells)
 
                 # Assert
                 expect(result).to eq(player)
             end
+        end
+        
+        it "can detect when a player has won with a vertical row" do
+            # Arrange
+            winning_grid = [["X", "", ""],
+                            ["X", "", ""],
+                            ["X", "", ""]]
+
+            # Act
+            result = TicTacToeLogic.new.get_winner(winning_grid)
+
+            # Assert
+            expect(result).to eq("X")
         end
     end
 end
