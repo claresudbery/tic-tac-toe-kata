e6c4f75c825a2bce16d976f51795f4f28a4a2d11
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-10-25 15:52:13 +0000

    Can now detects wins on the second row.

--- lib/tictactoe_logic.rb
+++ lib/tictactoe_logic.rb
@@ -1,8 +1,25 @@
 class TicTacToeLogic
     def get_winner(grid_cells)
-        if (grid_cells[0][0] == grid_cells[0][1]) \
+        if !grid_cells[0][0].nil? && !grid_cells[0][0].empty? \
+                && (grid_cells[0][0] == grid_cells[0][1]) \
                 && (grid_cells[0][1] == grid_cells[0][2])
             grid_cells[0][0]
-        end
+        elsif !grid_cells[1][0].nil? && !grid_cells[1][0].empty? \
+                && (grid_cells[1][0] == grid_cells[1][1]) \
+                && (grid_cells[1][1] == grid_cells[1][2])
+            grid_cells[1][0]
+        end  
+
+        # puts "*******************************************************"
+        # puts "grid_cells[0][0]: " + grid_cells[0][0]
+        # puts "grid_cells[0][1]: " + grid_cells[0][1]
+        # puts "grid_cells[0][2]: " + grid_cells[0][2]
+        # puts "grid_cells[1][0]: " + grid_cells[1][0]
+        # puts "grid_cells[1][1]: " + grid_cells[1][1]
+        # puts "grid_cells[1][2]: " + grid_cells[1][2]
+        # puts "grid_cells[2][0]: " + grid_cells[2][0]
+        # puts "grid_cells[2][1]: " + grid_cells[2][1]
+        # puts "grid_cells[2][2]: " + grid_cells[2][2]
+        # puts "*******************************************************"
     end
 end
