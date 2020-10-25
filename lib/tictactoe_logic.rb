192f3e21e079eb28374c3627bc5e1805a0b60c63
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-10-25 16:41:32 +0000

    Refactor: Extract method

--- lib/tictactoe_logic.rb
+++ lib/tictactoe_logic.rb
@@ -4,18 +4,23 @@ class TicTacToeLogic
         diagonals = [[rows[0][0], rows[1][1], rows[2][2]],
                      [rows[0][2], rows[1][1], rows[2][0]]]
         all_lines = rows + columns + diagonals
+
+        find_winner_in_all_lines(all_lines)
+    end
+
+    private 
+
+    def find_winner_in_all_lines(lines)
         winner = nil
         i = 0
 
-        while winner == nil && i < all_lines.length 
-            winner = find_winner_in_line(all_lines[i])
+        while winner == nil && i < lines.length 
+            winner = find_winner_in_line(lines[i])
             i = i + 1
         end
 
         winner
     end
-
-    private 
 
     def find_winner_in_line(grid_line)
         if !grid_line[0].nil? && !grid_line[0].empty? \
