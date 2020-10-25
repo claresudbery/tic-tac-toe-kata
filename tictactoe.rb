72c56ca328edca016e0eaa8c2967ecde00266310
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-10-25 18:28:32 +0000

    Refactor: Extract method

--- tictactoe.rb
+++ tictactoe.rb
@@ -54,13 +54,17 @@ class MyApp < Sinatra::Base
         decide_ai_aymbol
     end
 
+    def nil_or_empty(string)
+        string.nil? || string.empty?
+    end
+
     def decide_ai_aymbol
         num_inputs = 0
         for row in 0..2 
             for col in 0..2 
                 input = params["row#{row}_col#{col}_in"]
-                num_inputs = (input.nil? || input.empty?) ? num_inputs : num_inputs + 1
-                first_input = (first_input.nil? || first_input.empty?) ? input : first_input
+                num_inputs = nil_or_empty(input) ? num_inputs : num_inputs + 1
+                first_input = nil_or_empty(first_input) ? input : first_input
             end
         end
         set_ai_symbol(num_inputs, first_input)
