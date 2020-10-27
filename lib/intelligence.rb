6c0a8ec86734e5864b286882a54f05cb1920f087
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-10-27 12:09:19 +0000

    Refactor: Clean code.

--- lib/intelligence.rb
+++ lib/intelligence.rb
@@ -4,6 +4,12 @@ require_relative './full_grid_error'
 class Intelligence
     def choose_move(grid, next_player)
         empty_spaces = Grid::empty_spaces(grid)
+        first_empty_space(empty_spaces)
+    end
+
+    private 
+
+    def first_empty_space(empty_spaces)
         if empty_spaces.empty?
             raise FullGridError.new
         else
