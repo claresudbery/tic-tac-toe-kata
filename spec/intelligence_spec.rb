fef6291ebeaa00cf250ae7e4086a5be5469b3c84
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-10-27 10:57:28 +0000

    Added failing test cases for playing in the last available spot.

--- spec/intelligence_spec.rb
+++ spec/intelligence_spec.rb
@@ -3,17 +3,22 @@ require_relative '../lib/intelligence'
 
 RSpec.describe 'The Intelligence class' do
     context "deciding where to play next" do 
-        it "will choose to play in the only empty space left" do
-            # Arrange
-            grid_cells = [["O", "O", "X"],
-                          ["O", "O", "X"],
-                          ["X", "X", ""]]
+        # Arrange                
+        grids_with_only_one_space = {
+            [0,0] => [["", "O", "X"],["O", "X", "O"],["O", "X", "O"]],
+            [1,0] => [["X", "O", "X"],["", "X", "O"],["O", "X", "O"]],
+            [0,2] => [["X", "O", ""],["O", "X", "O"],["O", "X", "O"]],
+            [2,2] => [["X", "O", "X"],["O", "X", "O"],["O", "X", ""]]
+        }
 
-            # Act
-            result = Intelligence.new.choose_move(grid_cells, "O")
+        grids_with_only_one_space.each do |space_coords, grid_cells|
+            it "will choose to play in the only empty space left: #{space_coords}" do
+                # Act
+                result = Intelligence.new.choose_move(grid_cells, "O")
 
-            # Assert
-            expect(result).to eq([2,2])
+                # Assert
+                expect(result).to eq(space_coords)
+            end
         end
     end
 end
