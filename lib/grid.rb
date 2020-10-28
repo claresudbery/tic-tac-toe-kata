09dda9d830cef33bff6609bd4c11dca44a4adbed
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-10-28 13:52:21 +0000

    Refactor: Corrected a confusion between row/column vs x/y.

--- lib/grid.rb
+++ lib/grid.rb
@@ -14,10 +14,10 @@ class Grid
     def self.empty_spaces_no_exception(grid)
         empty_cells = []
 
-        grid.each_with_index do |column, column_index|
-            column.each_with_index do |symbol, row_index|
+        grid.each_with_index do |row, row_index|
+            row.each_with_index do |symbol, column_index|
                 if Utils::nil_or_empty?(symbol)
-                    empty_cells << [row_index, column_index]
+                    empty_cells << [column_index, row_index]
                 end
             end
         end
